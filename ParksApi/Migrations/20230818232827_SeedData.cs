0f554816623a81a8b8538ed6c7a150db2d250e5c
JaseGrable <jase.grable@gmail.com>
2023-08-18 16:28:56 -0700

    Add Seed Data

---
+++ ParksApi/Migrations/20230818232827_SeedData.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParksApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "Name", "State", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes.", "Acadia National Park", "Maine", "National" },
+                    { 2, "The southernmost national park is on three Samoan islands in the South Pacific. It protects coral reefs, rainforests, volcanic mountains, and white beaches.", "American Samoa", "American Samoa", "National" },
+                    { 3, "This site features more than 2,000 natural sandstone arches.", "Arches National Park", "Utah", "National" },
+                    { 4, "They contain the largest known assemblage of late Eocene and Oligocene mammal fossils.", "Badlands National Park", "South Dakota", "National" },
+                    { 5, "Its main attraction is backcountry recreation in the arid Chisos Mountains and in canyons along the river", "Big Bend National Park", "Texas", "National" },
+                    { 6, "A remote oasis catering to camping and boating on the shore of a scenic desert canyon reservoir.", "Lake Owyhee State Park", "Oregon", "State" },
+                    { 7, "Hat Rock is a geological formation that, along with another outcropping rock in the park called Boat Rock, are thought to be exposed remnants of a 12-million-year-old basalt flow.", "Hat Rock State Park", "Oregon", "State" },
+                    { 8, "Tumalo State Park is located in the Deschutes River canyon, running along both sides of the river.", "Tumalo State Park", "Oregon", "State" },
+                    { 9, "It covers 1,842 acres (7.45 km2) of land around 1,759-foot (536 m) Humbug Mountain, one of the tallest headlands on the Oregon coast.", "Humbug Mountain State Park", "Oregon", "State" },
+                    { 10, "Cape Blanco extends farther west than any point of land in the contiguous United States (lower 48 states) except portions of the Olympic Peninsula in Washington.", "Cape Blanco State Park", "Oregon", "State" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 10);
+        }
+    }
+}
--- ParksApi/Models/ParksApiContext.cs
+++ ParksApi/Models/ParksApiContext.cs
@@ -9,5 +9,23 @@ namespace ParksApi.Models
         public ParksApiContext(DbContextOptions<ParksApiContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Park>()
+                .HasData(
+                    new Park { ParkId = 1, Name = "Acadia National Park", State = "Maine", Type = "National", Description = "Acadia features the tallest mountain on the Atlantic coast of the United States, granite peaks, ocean shoreline, woodlands, and lakes." },
+                    new Park { ParkId = 2, Name = "American Samoa", State = "American Samoa", Type = "National", Description = "The southernmost national park is on three Samoan islands in the South Pacific. It protects coral reefs, rainforests, volcanic mountains, and white beaches." },
+                    new Park { ParkId = 3, Name = "Arches National Park", State = "Utah", Type = "National", Description = "This site features more than 2,000 natural sandstone arches." },
+                    new Park { ParkId = 4, Name = "Badlands National Park", State = "South Dakota", Type = "National", Description = "They contain the largest known assemblage of late Eocene and Oligocene mammal fossils." },
+                    new Park { ParkId = 5, Name = "Big Bend National Park", State = "Texas", Type = "National", Description = "Its main attraction is backcountry recreation in the arid Chisos Mountains and in canyons along the river" },
+
+                    new Park { ParkId = 6, Name = "Lake Owyhee State Park", State = "Oregon", Type = "State", Description = "A remote oasis catering to camping and boating on the shore of a scenic desert canyon reservoir." },
+                    new Park { ParkId = 7, Name = "Hat Rock State Park", State = "Oregon", Type = "State", Description = "Hat Rock is a geological formation that, along with another outcropping rock in the park called Boat Rock, are thought to be exposed remnants of a 12-million-year-old basalt flow." },
+                    new Park { ParkId = 8, Name = "Tumalo State Park", State = "Oregon", Type = "State", Description = "Tumalo State Park is located in the Deschutes River canyon, running along both sides of the river." },
+                    new Park { ParkId = 9, Name = "Humbug Mountain State Park", State = "Oregon", Type = "State", Description = "It covers 1,842 acres (7.45 km2) of land around 1,759-foot (536 m) Humbug Mountain, one of the tallest headlands on the Oregon coast." },
+                    new Park { ParkId = 10, Name = "Cape Blanco State Park", State = "Oregon", Type = "State", Description = "Cape Blanco extends farther west than any point of land in the contiguous United States (lower 48 states) except portions of the Olympic Peninsula in Washington." }
+                );
+        }
     }
 }
