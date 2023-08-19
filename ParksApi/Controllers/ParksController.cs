245b9dc539c245ebd7213ee88df401794ea2d1df
JaseGrable <jase.grable@gmail.com>
2023-08-18 17:00:17 -0700

    Add GET methods

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParksApi.Models;
+
+namespace ParksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParksController : ControllerBase
+    {
+        private readonly ParksApiContext _db;
+
+        public ParksController(ParksApiContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/parks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Park>>> Get()
+        {
+            return await _db.Parks.ToListAsync();
+        }
+
+        // GET api/parks/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Park>> GetPark(int id)
+        {
+            Park park = await _db.Parks.FindAsync(id);
+            if (park == null)
+            {
+                return NotFound();
+            }
+            return park;
+        }
+    }
+}
